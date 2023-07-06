1b8414ee08cf71d7395c1eb0074f64648ae933d8
Aditya Rao <aditya.g.rao@outlook.com>
2023-07-06 18:18:48 -0400

    Add this as well

---
+++ WebHelpers/src/main/java/com/raos/util/webhelpers/criteria/NotCriteria.java
@@ -0,0 +1,24 @@
+package com.raos.util.webhelpers.criteria;
+
+//Represents NOT notation on criterias
+class NotCriteria implements Criteria {
+	private static final long serialVersionUID = 1L;
+
+	// Criterias
+	private final Criteria criteria;
+
+	// Ands the criterias
+	public NotCriteria(Criteria criteria) {
+		this.criteria = criteria;
+	}
+
+	// getters
+	public Criteria getCriteria() {
+		return criteria;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("NOT (%s)", criteria);
+	}
+}
---
+++ WebHelpers/src/main/java/com/raos/util/webhelpers/data/DatabaseSession.java
@@ -0,0 +1,49 @@
+package com.raos.util.webhelpers.data;
+
+import java.io.Serializable;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Map;
+
+import com.raos.util.webhelpers.criteria.Criteria;
+
+// Acts as a session or connection to the database
+public class DatabaseSession implements AutoCloseable {
+
+	// Fields
+	private Map<Class<?>, DatabaseRepository<?>> repositories;
+	private Connection connection;
+
+	public DatabaseSession(DatabaseService service) throws SQLException {
+		connection = service.openConnection();
+	}
+
+	// Getting a database repository
+	@SuppressWarnings("unchecked")
+	public <T extends Serializable> DatabaseRepository<T> getRepository(Class<T> clazz) {
+		// Check whether the repository contains the class associated
+		if (repositories.containsKey(clazz))
+			return (DatabaseRepository<T>) repositories.get(clazz);
+
+		// Otherwise create a repository of that class
+		return null;
+	}
+
+	// Creates a new query
+	public Query createQuery(String sql) {
+		return QueryBuilder.create(this).setCommand(sql).build();
+	}
+
+	// TODO: Fix so that its a Columns class instead of a string array
+	// Creates a new query
+	public Query createQuery(Columns columns, Criteria criteria) {
+		return QueryBuilder.create(this).setCriteria(columns, criteria).build();
+	}
+
+	@Override
+	public void close() throws Exception {
+		if (!connection.isClosed())
+			connection.close();
+	}
+
+}
