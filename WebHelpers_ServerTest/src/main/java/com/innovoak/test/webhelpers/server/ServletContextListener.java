d9887eb9cf1035d2d8ec739f04d45bd0b9113ad1
adityarao2005 <82911340+adityarao2005@users.noreply.github.com>
2023-07-28 13:18:30 -0400

    Adding context listener

---
+++ WebHelpers_ServerTest/src/main/java/com/innovoak/test/webhelpers/server/ServletContextListener.java
@@ -0,0 +1,21 @@
+package com.innovoak.test.webhelpers.server;
+
+import javax.servlet.annotation.WebListener;
+
+import com.innovoak.util.webhelpers.data.Configuration;
+import com.innovoak.util.webhelpers.data.Configuration.ConfigurationBuilder;
+import com.innovoak.util.webhelpers.server.DatabaseContextListener;
+
+@WebListener
+public class ServletContextListener extends DatabaseContextListener {
+
+    @Override
+    public Configuration getConfiguration() {
+        return ConfigurationBuilder.create().setDriverClassName("com.mysql.cj.jdbc.Driver")
+                .setUrl("jdbc:mysql://localhost:3306/homework_database").setUsername("root").setPassword("root")
+                .setDefaultAutoCommit(false).setMaxIdle(15).setMinIdle(10).setMaxOpenPreparedStatements(150)
+                .setPoolPreparedStatements(true).build();
+    }
+
+}
+
